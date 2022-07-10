4decc3941f5963ad733f58484e6334bdec7315b9
Andrej Zevzikov <andrejzevz13@gmail.com>
2022-07-11 01:04:51 +0300

    first commit just empty project

---
+++ src/main/java/com/final_project/daily_operations/DailyOperationsApplication.java
@@ -0,0 +1,13 @@
+package com.final_project.daily_operations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyOperationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailyOperationsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/final_project/daily_operations/DailyOperationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.final_project.daily_operations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyOperationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
