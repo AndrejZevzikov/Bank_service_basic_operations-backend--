9a88c8edd4d5fa9daf22501c91df1d3feab024dd
Andrej Zevzikov <andrejzevz13@gmail.com>
2022-07-11 01:12:45 +0300

    second commit for testing

--- src/main/java/com/final_project/daily_operations/DailyOperationsApplication.java
+++ src/main/java/com/final_project/daily_operations/DailyOperationsApplication.java
@@ -8,7 +8,7 @@ public class DailyOperationsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DailyOperationsApplication.class, args);
-		System.out.println("Hello");
+		System.out.println("Hello Andrew");
 	}
 
 }
